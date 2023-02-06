c1c561139f0b0e86b7546fee5187e61c9dff2f30
Jacob Greenlee <jgreenlee@interstatecapital.onmicrosoft.com>
2023-02-06 14:08:12 -0600

    commit

--- my-website/lighthouse/pup-naivation.js
+++ my-website/lighthouse/pup-naivation.js
@@ -4,17 +4,26 @@ import puppeteer from 'puppeteer';
 import {startFlow} from 'lighthouse/lighthouse-core/fraggle-rock/api.js';
 
 async function captureReport() {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-
-  const flow = await startFlow(page, {name: 'Single Navigation'});
-  await flow.navigate('https://web.dev/performance-scoring/');
-
-  await browser.close();
-
-  const report = await flow.generateReport();
-  fs.writeFileSync('flow.report.html', report);
-  open('flow.report.html', {wait: false});
-}
-
-captureReport();
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+  
+    const testUrl = 'https://genius.com';
+    const flow = await startFlow(page, {name: 'Cold and warm navigations'});
+    await flow.navigate(testUrl, {
+      stepName: 'Cold navigation'
+    });
+    await flow.navigate(testUrl, {
+      stepName: 'Warm navigation',
+      configContext: {
+        settingsOverrides: {disableStorageReset: true},
+      },
+    });
+  
+    await browser.close();
+  
+    const report = await flow.generateReport();
+    fs.writeFileSync('flow.report.html', report);
+    open('flow.report.html', {wait: false});
+  }
+  
+  captureReport();
